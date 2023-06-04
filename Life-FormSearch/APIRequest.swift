8adc5103ced19f8abb438345eec135151a2475ff
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-06-04 00:52:07 -0300

    Project finished

---
+++ Life-FormSearch/APIRequest.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+protocol APIRequest {
+    associatedtype Response
+    
+    var urlRequest: URLRequest { get }
+    
+    func decodeResponse(data: Data) throws -> Response
+}
+
+enum APIRequestError: Error {
+    case itemNotFound
+}
---
+++ Life-FormSearch/EOLHierarchy.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct Ancestor : Codable{
+    var scientificName: String
+    var taxonRank: String?
+
+}
+
+struct EOLHierarchy : Codable{
+    var ancestors: [Ancestor]?
+}
